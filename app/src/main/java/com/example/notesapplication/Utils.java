ccee6cb9f9514ac44a9091e64b670bdada6c745d
denisabolba <denisabolba27@gmail.com>
2023-07-04 18:03:49 +0300

    initial

--- app/src/main/java/com/example/notesapplication/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.example.notesapplication;
-
-public class Utils {
-    public static final  String EMAIL = "denisabolba27@gmail.com";
-    public static final String PASSWORD = "hubiaxylhztnxzhu";
-}
-
